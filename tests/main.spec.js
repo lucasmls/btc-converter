15bdaba364fa5a458077ea40fd77a6d95821172e
Lucas Mendes <lucaslsbh1@gmail.com>
2017-12-01 19:06:42 -0200

    Created build and npm link

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -6,11 +6,20 @@ const btcConverter = 'node.exe ./src/main.js';
 const pkg = require('../package.json');
 
 describe('Main CLI', () => {
-  it('should return the version of btc-converter ', (done) => {
+  it('should return the version of btc-converter', (done) => {
     exec(`${btcConverter} --version`, (err, stdout, stderr) => {
       if(err) throw err;
       expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
       done();
     });
   });
+
+  it('should return the description when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.includes('Convert Bitcoin to any currency defined')).to.be.true;
+      done();
+    });
+  });
+
 });
