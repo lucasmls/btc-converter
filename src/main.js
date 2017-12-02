2648c5428b2c39299023381240ec16e87d6e30be
Lucas Mendes <lucaslsbh1@gmail.com>
2017-12-01 22:49:59 -0200

    Add options to CLI

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@
 
 const program = require('commander');
 const pkg = require('../package.json');
+const convertBTC = require('./ConvertBTC');
 
 program
   .version(pkg.version)
   .description('Convert Bitcoin to any currency defined.')
+  .option('-h, --help', 'output usage information')
+  .option('-C, --currency <currency>', 'Currency to be converted. (Default: USD)')
+  .option('-A, --amount <amount>', 'Value in Bitcoin to convert. (Default: 1)')
   .parse(process.argv);
+
+console.log(convertBTC(program.currency, program.amount));
