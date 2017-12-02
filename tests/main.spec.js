39c1176348185c7ef5db3d5622cf83085762e440
Lucas Mendes <lucaslsbh1@gmail.com>
2017-12-01 23:28:48 -0200

    Using the API

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -6,9 +6,9 @@ const btcConverter = 'node.exe ./src/main.js';
 const pkg = require('../package.json');
 
 describe('Main CLI', () => {
-  it('should return the version of btc-converter', (done) => {
+  it('should return version of btc-converter', (done) => {
     exec(`${btcConverter} --version`, (err, stdout, stderr) => {
-      if(err) throw err;
+      if (err) throw err;
       expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
       done();
     });
@@ -16,15 +16,15 @@ describe('Main CLI', () => {
 
   it('should return the description when btc-converter --help', (done) => {
     exec(`${btcConverter} --help`, (err, stdout, stderr) => {
-      if(err) throw err;
-      expect(stdout.includes('Convert Bitcoin to any currency defined')).to.be.true;
+      if (err) throw err;
+      expect(stdout.includes('Convert Bitcoint to any currency defined')).to.be.true;
       done();
     });
   });
 
   it('should return the currency option when btc-converter --help', (done) => {
     exec(`${btcConverter} --help`, (err, stdout, stderr) => {
-      if(err) throw err;
+      if (err) throw err;
       expect(stdout.includes('--currency')).to.be.true;
       done();
     });
@@ -32,10 +32,9 @@ describe('Main CLI', () => {
 
   it('should return the amount option when btc-converter --help', (done) => {
     exec(`${btcConverter} --help`, (err, stdout, stderr) => {
-      if(err) throw err;
+      if (err) throw err;
       expect(stdout.includes('--amount')).to.be.true;
       done();
     });
   });
-
 });
